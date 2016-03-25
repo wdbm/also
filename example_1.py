5339af7748b29a570ddba0b9f2d346d077e40047
Will Breaden Madden <w.bm@cern.ch>
2016-03-25 17:26:38 +0100

    change camel case to snake case, tidy

--- example_1.py
+++ example_1.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 ################################################################################
 #                                                                              #
 # also                                                                         #
@@ -20,7 +22,7 @@
 #                                                                              #
 # This program is distributed in the hope that it will be useful, but WITHOUT  #
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        #
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for    #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for     #
 # more details.                                                                #
 #                                                                              #
 # For a copy of the GNU General Public License, see                            #
@@ -28,23 +30,28 @@
 #                                                                              #
 ################################################################################
 
-class AccumulatorLogicalAnd(object):
+name    = "also"
+version = "2016-03-25T1619Z"
+
+class Accumulator_logical_and(object):
     def __init__(self):
-        self.attribute_all = True
-        self.attribute_any = False
-        self.attribute_none = None
-        self.attribute_numberTotal = 0
-        self.attribute_numberPassed = 0
-        self.attribute_numberFailed = 0
+        self.attribute_all           = True
+        self.attribute_any           = False
+        self.attribute_none          = None
+        self.attribute_number_total  = 0
+        self.attribute_number_passed = 0
+        self.attribute_number_failed = 0
     def also(self, condition):
-        self.attribute_all = self.attribute_all and condition
-        self.attribute_any = self.attribute_any or condition
-        self.attribute_none = not condition and (self.attribute_none is None or self.attribute_none)
-        self.attribute_numberTotal += 1
+        self.attribute_all           = self.attribute_all and condition
+        self.attribute_any           = self.attribute_any or condition
+        self.attribute_none          = not condition and \
+                                       (self.attribute_none is None or \
+                                       self.attribute_none)
+        self.attribute_number_total  += 1
         if condition:
-            self.attribute_numberPassed += 1
+            self.attribute_number_passed += 1
         else:
-            self.attribute_numberFailed += 1
+            self.attribute_number_failed += 1
         return condition
     def all(self):
         return self.attribute_all
@@ -53,11 +60,11 @@ class AccumulatorLogicalAnd(object):
     def none(self):
         return self.attribute_none
     def total(self):
-        return self.attribute_numberTotal
+        return self.attribute_number_total
     def passed(self):
-        return self.attribute_numberPassed
+        return self.attribute_number_passed
     def failed(self):
-        return self.attribute_numberFailed
+        return self.attribute_number_failed
 
 def action1():
     print("action 1")
@@ -70,20 +77,20 @@ def action3():
 
 def main():
 
-    accumulatorLogicalAnd = AccumulatorLogicalAnd()
-    also = accumulatorLogicalAnd.also
-    all = accumulatorLogicalAnd.all
-    any = accumulatorLogicalAnd.any
-    none = accumulatorLogicalAnd.none
-    total = accumulatorLogicalAnd.total
-    passed = accumulatorLogicalAnd.passed
-    failed = accumulatorLogicalAnd.failed
+    accumulator_logical_and = Accumulator_logical_and()
+    also                    = accumulator_logical_and.also
+    all                     = accumulator_logical_and.all
+    any                     = accumulator_logical_and.any
+    none                    = accumulator_logical_and.none
+    total                   = accumulator_logical_and.total
+    passed                  = accumulator_logical_and.passed
+    failed                  = accumulator_logical_and.failed
 
     condition1 = True
     condition2 = False
     condition3 = True
 
-    print
+    print("")
 
     if also(condition1):
         action1()
@@ -96,14 +103,14 @@ def main():
     else:
         print("some conditions failed")
 
-    print
-    print("Did all pass?\t\t{result}".format(result = str(all())))
-    print("Did any pass?\t\t{result}".format(result = str(any())))
-    print("Did none pass?\t\t{result}".format(result = str(none())))
+    print("")
+    print("Did all pass?\t\t{result}".format(result    = str(all())))
+    print("Did any pass?\t\t{result}".format(result    = str(any())))
+    print("Did none pass?\t\t{result}".format(result   = str(none())))
     print("How many in total?\t{result}".format(result = str(total())))
-    print("How many passed?\t{result}".format(result = str(passed())))
-    print("How many failed?\t{result}".format(result = str(failed())))
-    print
+    print("How many passed?\t{result}".format(result   = str(passed())))
+    print("How many failed?\t{result}".format(result   = str(failed())))
+    print("")
 
 if __name__ == '__main__':
     main()
